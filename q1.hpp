55dd8c97e49b177b3bafda69f2781be3a6fbea74
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-13 11:09:49 -0700

    Update q1.hpp

--- q1.hpp
+++ q1.hpp
@@ -1,1 +1,12 @@
+#ifndef Q1_HPP
+#define Q1_HPP
 
+#include <iostream>
+
+// Function to get two integer values from the user
+void getTwoValues(int &begin, int &end) {
+    do {
+        std::cout << "Enter two integer values (begin < end): ";
+        std::cin >> begin >> end;
+    } while (begin >= end);
+}
