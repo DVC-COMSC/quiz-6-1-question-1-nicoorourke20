58adeec220a55560d6f65bcad4f6b77c57a581b3
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-13 11:56:05 -0700

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -3,6 +3,43 @@
 
 #include <iostream>
 
+// Function to get two integer values from the user
+void getTwoValues(int &begin, int &end) {
+    do {
+        std::cout << "Enter two integer values (begin < end): ";
+        std::cin >> begin >> end;
+    } while (begin >= end);
+}
+
+// Function to check if a number is prime
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+// Function to find the next prime number after a given number
+int getNextPrime(int begin) {
+    int next = begin + 1;
+    while (!isPrime(next)) {
+        next++;
+    }
+    return next;
+}
+
+// Function to find the previous prime number before a given number
+int getPrevPrime(int end) {
+    int prev = end - 1;
+    while (!isPrime(prev)) {
+        prev--;
+    }
+    return prev; 
+}
+
 void getTwoValues(int &begin, int &end);
 int getNextPrime(int begin);
 int getPrevPrime(int end);
