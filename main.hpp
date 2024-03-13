e75a5b229d71a2bc3a9a1664dd6d311ab3ebe583
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-13 11:06:10 -0700

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -1,5 +1,10 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
 #include <iostream>
 
 void getTwoValues(int &begin, int &end);
 int getNextPrime(int begin);
 int getPrevPrime(int end);
+
+#endif // MAIN_HPP
